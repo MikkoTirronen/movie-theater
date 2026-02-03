77aa5933e515ca1ca931e1224e2dfbe5941f3aa1
Mikko Tirronen <mikkoatirronen@gmail.com>
2026-02-03 12:54:33 +0100

    modified Theater components to use seat object

--- src/components/TheaterRowSeat.tsx
+++ src/components/TheaterRowSeat.tsx
@@ -1,16 +1,15 @@
 import { type seat } from "../data/theaterStatus";
 import { useMovieContext } from "../context/useMovieContext";
 type TheaterRowSeatProps = {
-    seatStatus: seat;
+  seatStatus: seat;
 };
 export default function TheaterRowSeat({ seatStatus }: TheaterRowSeatProps) {
-    const { selectSeat } = useMovieContext();
-    const currentSeat = seatStatus.seat
-    return (
+  const { selectSeat } = useMovieContext();
+  return (
     <div
-      className={`seat ${seatStatus.status === "occupied" && "occupied"} ${seatStatus.status==="selected"&& "selected"}`}
-            onClick={() => {
-                selectSeat(currentSeat);
+      className={`seat ${seatStatus.status === "occupied" && "occupied"} ${seatStatus.status === "selected" && "selected"}`}
+      onClick={() => {
+        selectSeat(seatStatus);
         console.log(seatStatus.seat);
       }}
     ></div>
