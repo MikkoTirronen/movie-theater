a968f2ba56265e50a204163c25b8a813c1304bd4
Mikko Tirronen <mikkoatirronen@gmail.com>
2026-02-03 12:53:17 +0100

    added onChange to movieSelect

--- src/components/MovieSelect.tsx
+++ src/components/MovieSelect.tsx
@@ -1,14 +1,22 @@
 import { useMovieContext } from "../context/useMovieContext";
 
 export default function MovieSelect() {
-  const { movies } = useMovieContext();
+  const { movies, setPrice,  } = useMovieContext();
+
   return (
     <>
       <label htmlFor="movie">Pick a movie:</label>
-      <select name="movie" id="movie">
+      <select
+        name="movie"
+        id="movie"
+        onChange={(e) => {
+          setPrice(e.target.value)
+        }}
+      >
         {movies.map((movie) => {
           return (
             <option
+              key={"movie" + movie.id}
               value={movie.price}
             >{`${movie.title} (${movie.price} kr)`}</option>
           );
