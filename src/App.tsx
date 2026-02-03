7d46b6538801ba05fca44ceb0265c9a351cd6985
Mikko Tirronen <mikkoatirronen@gmail.com>
2026-02-03 13:23:57 +0100

    added fetchMovies inside a useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
+import { useEffect } from "react";
 import "./App.css";
 import MovieSelect from "./components/MovieSelect";
 import Theater from "./components/Theater";
 import TotalPrice from "./components/TotalPrice";
+import { useMovieContext } from "./context/useMovieContext";
 
 function App() {
+  const { setMovies } = useMovieContext();
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/movies");
+        const data = await response.json();
+        setMovies(data);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    };
+    fetchMovies();
+  }, [setMovies]);
+
   return (
     <>
-      
       <MovieSelect />
       <Theater />
-      <TotalPrice/>
+      <TotalPrice />
     </>
   );
 }
