c818356a49acfa6e7c1bedf56fcd74419f1cded1
Mikko Tirronen <mikkoatirronen@gmail.com>
2026-02-12 13:54:00 +0100

    updated movieSelect to work off of id not index

--- src/components/MovieSelect.tsx
+++ src/components/MovieSelect.tsx
@@ -3,22 +3,25 @@ import { useMovieContext } from "../context/useMovieContext";
 export default function MovieSelect() {
   const { movies, setCurrentMovie, setSelectedSeats } = useMovieContext();
 
+  const handleOnChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedSeats([]);
+    const selectedMovie = movies.findIndex((movie) => movie.id === e.target.value);
+    setCurrentMovie(movies[selectedMovie]);
+  }
+
   return (
     <div className="movie-container">
       <label htmlFor="movie">Pick a movie:</label>
       <select
         name="movie"
         id="movie"
-        onChange={(e) => {
-          setSelectedSeats([]);
-          setCurrentMovie(movies[parseInt(e.target.value)]);
-        }}
+        onChange={handleOnChange}
       >
-        {movies.map((movie, index) => {
+        {movies.map((movie) => {
           return (
             <option
               key={"movie" + movie.id}
-              value={index}
+              value={movie.id}
             >{`${movie.title} (${movie.price} kr)`}</option>
           );
         })}
